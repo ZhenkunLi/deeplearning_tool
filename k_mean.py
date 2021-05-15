4249232e2997487b8429adc0030e838f56b22eab
Richard <55397305+Richardstudypython@users.noreply.github.com>
2021-05-15 13:03:25 +0800

    update

--- k_mean.py
+++ k_mean.py
@@ -1,4 +1,5 @@
 print(k_mean)
+<<<<<<< HEAD
 print('hello world')
 def kmean(x,y,z=5)
 master branch
@@ -13,3 +14,5 @@ print(52333)
 print(52333)
 print('1221euoihfajdlkshfljk')
 >>>>>>> parent of a6b54c9 (Update k_mean.py)
+=======
+>>>>>>> parent of 315cf77 (Update k_mean.py)
