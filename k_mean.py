2018805c27e9f6c9d34fcd1e22417afd37d345b2
Richard <>
2021-05-15 13:48:22 +0800

    update

--- k_mean.py
+++ k_mean.py
@@ -1,5 +1,4 @@
 print(k_mean)
-<<<<<<< HEAD
 print('hello world')
 def kmean(x,y,z=5)
 master branch
@@ -8,11 +7,5 @@ print('kkk,2020.05.09')
 
 print('kkk,20200510')
 
-<<<<<<< HEAD
-print(52333)
-=======
 print(52333)
 print('1221euoihfajdlkshfljk')
->>>>>>> parent of a6b54c9 (Update k_mean.py)
-=======
->>>>>>> parent of 315cf77 (Update k_mean.py)
